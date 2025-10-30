11a0f5c6a2b43ca5604982cac79942c0c0069740
Andrewfpai <nicolasandrew.28@gmail.com>
2025-10-30 20:20:56 +0800

    Fix Typescript any type error 4

--- frontend/app/students/[studentsId]/edit/page.tsx
+++ frontend/app/students/[studentsId]/edit/page.tsx
@@ -1,39 +1,18 @@
-
-
-
-// import StudentList from "./components/StudentList";
-// import StudentForm from "./components/StudentForm";
-
-
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { Separator } from "@/components/ui/separator";
-
 import Info from "./Info";
 import { getStudentById } from "@/lib/api";
 
-import { PageProps } from "next"; // optional, if needed
-
-type Props = {
-  params: { studentsId: string };
-};
-
-
 export default async function Page({ params }: { params: { studentsId: string } }) {
-    // console.log("TYPE", typeof(params))
-    // const resolvedParams = params;
-    const studentId = parseInt(params.studentsId, 10);
-    const student = await getStudentById(studentId.toString());
+    const student = await getStudentById(params.studentsId);
+
     return (
-    <div className="flex flex-col px-8 mt-4">
-
-        <div className="flex flex-row gap-2 items-center mb-4">
-            <SidebarTrigger className="py-2 -ml-2"/>
-            <div className="w-px h-5 bg-gray-300 mx-2"></div>
-            <div className="font-semibold text-lg">Students Database</div>
-        </div>
-          
+        <div className="flex flex-col px-8 mt-4">
+            <div className="flex flex-row gap-2 items-center mb-4">
+                <SidebarTrigger className="py-2 -ml-2"/>
+                <div className="w-px h-5 bg-gray-300 mx-2"></div>
+                <div className="font-semibold text-lg">Students Database</div>
+            </div>
             <Info student={student}/>
-
-    </div>
-  );
+        </div>
+    );
 }
